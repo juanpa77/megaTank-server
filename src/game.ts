65d9593084ffb3baf35b87138482e0a5647054e6
Juan <juanpa.box.77@gmail.com>
2024-05-07 17:51:18 -0300

    new feature/ add game and tank class

---
+++ src/game.ts
@@ -0,0 +1,18 @@
+import { Tank } from "./Tank";
+
+export class Game {
+  tanks: Tank[];
+
+  constructor() {
+    this.tanks = [];
+  }
+
+  addTank({ id, x, y }: { id: string, x: number, y: number }) {
+    const newTank = new Tank(id, x, y)
+    this.tanks.push(newTank)
+  }
+
+  deleteTank(id: string) {
+    this.tanks.filter(tank => tank.id !== id)
+  }
+}
